4888c618af852c5116c307abb8cd0f8f824701b0
nhan nguyen <nhannguyenmath95@gmail.com>
2016-06-24 11:52:01 +0700

    + create resources folder + create WEB-INF folder # move js folder to resources folder # move css folder to resources folder # move all img files to resources/img folder # move all html files to WEB-INF/views folder

---
+++ WebContent/resources/js/home.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+//                nav: true
+            },
+            600: {
+                items: 2
+//                nav: false
+            },
+            1000: {
+                items: 3
+//                nav: true
+//                loop: true
+            }
+        }
+    });
+
+    $('a.gallery').featherlightGallery({
+        gallery: {
+            previous: '«',
+            next: '»',
+            fadeIn: 300
+        },
+        openSpeed: 300
+    });
+
+    $('nav').find('a:not(.notScroll)').click(function (e) {
+        e.preventDefault();
+        var section = $(this).attr('href');
+        if (section == '#') {
+            $('html, body').animate({
+                scrollTop: 0
+            });
+        } else {
+              $('html, body').animate({
+                scrollTop: $(section).offset().top
+            });
+        }
+    });
+});
